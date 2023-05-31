63b070fcf6d9d300b68270e451942a5026a20d57
Nicolas <nicolas@nicolasf.uy>
2023-05-30 22:48:23 -0300

    under construction design

---
+++ website/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import img from "../assets/img/logo.png";
+
+export default function Home() {
+  return (
+    <main className="h-auto w-full text-center">
+      <div className="mt-20">
+        <Image
+          src={img}
+          alt="ENE Labs"
+          width={500}
+          height={500}
+          className="z-10 animate-pulse mx-auto w-auto"
+        />
+
+        <div className="mt-10">
+          <p>
+            <code> coming soon...stay tuned!</code>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
